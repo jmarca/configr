ac56e3eb497cffbb413811ed551013baeea241ed
James E. Marca <james@activimetrics.com>
2017-07-18 16:13:14 -0700

    go one up from first node_modules
    changed my mind.  install to the top level .Rlibs first

--- RinstallDeps.R
+++ RinstallDeps.R
@@ -11,7 +11,8 @@ dot_is <- getwd()
 
 plaba <- regexpr(pattern='node_modules',envrr['PWD'])
 if(plaba>0){
-    stoppoint <- plaba + 12 ## same as  attr(plaba,'match.length')
+    ## stoppoint <- plaba + 12 ## same as  attr(plaba,'match.length')
+    stoppoint <- plaba ## actually, go one up
     dot_is <- substr(envrr['PWD'],1,stoppoint)
 }
 print(paste('dot_is',dot_is))
