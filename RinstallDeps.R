9d0cb5b9327872eb256f1d70a8434c9ff40c1b46
James E. Marca <james@activimetrics.com>
2017-07-21 13:05:39 -0700

    update RinstallDeps to latest (With if statement)

--- RinstallDeps.R
+++ RinstallDeps.R
@@ -5,7 +5,7 @@ dot_is <- getwd()
 plaba <- regexpr(pattern='/node_modules',envrr['PWD'])
 if(plaba>0){
     ## stoppoint <- plaba + 12 ## same as  attr(plaba,'match.length')
-    stoppoint <- plaba - 1  ## actually, go one up
+    stoppoint <- plaba - 1 ## actually, go one up
     dot_is <- substr(envrr['PWD'],1,stoppoint)
 }
 node_paths <- dir(dot_is,pattern='\\.Rlibs',
@@ -20,7 +20,11 @@ if(!file.exists(path)){
 lib_paths <- .libPaths()
 .libPaths(c(path,node_paths,lib_paths))
 
-install.packages("roxygen2",repos="https://cloud.r-project.org")
-install.packages("devtools",repos="https://cloud.r-project.org")
-## devtools::install_github("hadley/devtools")
+if(length(find.package("roxygen2",quiet=TRUE)) == 0){
+    install.packages("roxygen2",repos="https://cloud.r-project.org",lib=path)
+}
+if(length(find.package("devtools",quiet=TRUE)) == 0){
+    install.packages("devtools",repos="https://cloud.r-project.org",lib=path)
+}
+devtools::install_github("hadley/devtools")
 devtools::install_deps()
